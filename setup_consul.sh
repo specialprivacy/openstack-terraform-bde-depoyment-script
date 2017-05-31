52e95251921e2dbc894a1689c7ca96fb96a54fc4
Cecile Tonglet <cecile.tonglet@gmail.com>
2017-05-31 16:14:26 +0200

    Simplified consul by running it on the system directly
    Running inside a container was overkill but the standard way with Docker
    Machine.

--- setup_consul.sh
+++ setup_consul.sh
@@ -2,14 +2,14 @@
 
 address=`ip route get 1 | awk '{print $NF;exit}'`
 
-# install Docker
-wget -qO - https://get.docker.com | sh -
-systemctl stop docker
-sed -i "s,-H fd://,-H fd:// -H tcp://0.0.0.0:2375," /lib/systemd/system/docker.service
+apt-get install -y --no-install-recommends unzip
+
+cd /tmp
+wget https://releases.hashicorp.com/consul/0.8.3/consul_0.8.3_linux_amd64.zip
+unzip -d /usr/local/sbin consul_0.8.3_linux_amd64.zip
+rm consul_0.8.3_linux_amd64.zip
+echo "CONSUL_OPTS=-server -bootstrap" >> /etc/environment
+wget -O /lib/systemd/system/consul.service https://gist.githubusercontent.com/cecton/52b08c398dbcc7e993052663adc78528/raw/15e3d42d9451e98486fb5040dc028104f5f9a003/consul.service
 systemctl daemon-reload
-systemctl start docker
-
-set -x
-
-# setup consul
-docker run -d -p 8500:8500 --name=consul progrium/consul -server -bootstrap
+systemctl enable consul
+systemctl start consul
